d838685f43b20fd6bae568eae724685e1af2225e
Austin Passy <367897+thefrosty@users.noreply.github.com>
2026-06-08 12:09:34 -0700

    Wrap with conditionals

--- views/install.blade.php
+++ views/install.blade.php
@@ -1,7 +1,13 @@
 cd "$HOME"
 
 # https://make.wordpress.org/cli/handbook/guides/installing/#recommended-installation
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+if ! curl -sS -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar; then
+    echo 'VITO_SSH_ERROR' && exit 1
+fi
+
+if ! chmod +x wp-cli.phar; then
+    echo 'VITO_SSH_ERROR' && exit 1
+fi
 
 # Quick verification using GPG:
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar.asc
@@ -12,7 +18,8 @@ if ! gpg --verify wp-cli.phar.asc wp-cli.phar; then
     echo 'VITO_SSH_ERROR' && exit 1
 fi
 
-chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
+if ! sudo mv wp-cli.phar /usr/local/bin/wp; then
+    echo 'VITO_SSH_ERROR' && exit 1
+fi
 
 wp --info
